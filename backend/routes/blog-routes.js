263a0654c387111e7fee7a676ae643cf614d0389
Kelvin Obidozie <kelvinobidozie@programmer.net>
2023-08-20 18:23:30 +0100

    Added get blog by userID

--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllBlogs, addBlog, updateBlog, getBlogByID, deleteBlog } from '../controllers/blog-controller';
+import { getAllBlogs, addBlog, updateBlog, getBlogByID, deleteBlog, getByUserID } from '../controllers/blog-controller';
 
 const blogRouter = express.Router();
 blogRouter.get('/', getAllBlogs);
@@ -7,6 +7,7 @@ blogRouter.post('/add', addBlog)
 blogRouter.put('/update/:id', updateBlog)
 blogRouter.delete('/:id',deleteBlog )
 blogRouter.get('/:id', getBlogByID )
+blogRouter.get('/user/:id', getByUserID )
 
 
 export default blogRouter;
