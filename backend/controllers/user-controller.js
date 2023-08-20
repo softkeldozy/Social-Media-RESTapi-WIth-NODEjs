59906f71ddee9608f66ccd464b7138da415b1d13
Kelvin Obidozie <kelvinobidozie@programmer.net>
2023-08-20 11:39:03 +0100

    Added sign functionality

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -1,5 +1,5 @@
 import User from "../model/user";
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcryptjs";
 
 export const getAllUser = async (req, res, next) => {
   let users;
@@ -31,7 +31,7 @@ export const signup = async (req, res, next) => {
   }
 
   // if no existing user exists, create a new one
-  const hashedPassword = bcryptjs.hashSync(password);
+  const hashedPassword = bcrypt.hashSync(password);
   const user = new User({
     name,
     email,
@@ -45,4 +45,28 @@ export const signup = async (req, res, next) => {
      return console.log(error);
     }
     return res.status(201).json({ user });
+};
+
+// Logging in 
+export const login = async (req, res, next) => {
+   const {email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({email});
+  } 
+  catch (error) {
+    return console.log(error); 
+  }
+  // check if user already exists
+  if(!existingUser){
+    return res.status(404).json({nessage: 'Couldn\'t find user with existin email address'});
+  }
+  // checking passwords for correctness
+  
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect){
+    return res.status(400).json({message: 'Invalid password'});
+  }
+  return res.status(200).json({message: 'Login successful'});
 };
